d67f4722325039fc52ca61fdb6e8077b02921fab
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2023-09-30 03:47:48 -0300

    first commit

---
+++ src/infra/modules/app.module.ts
@@ -0,0 +1,9 @@
+import { AppService } from '@application/services/app.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}

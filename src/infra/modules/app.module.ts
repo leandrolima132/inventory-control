88c5fb13bca3caffa8dc4d022b8db598dee579f0
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2023-09-30 04:11:18 -0300

    feat: initial database config - Prisma

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -1,9 +1,9 @@
 import { AppService } from '@application/services/app.service';
 import { Module } from '@nestjs/common';
+import { DataBaseModule } from './database.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [DataBaseModule],
   providers: [AppService],
 })
 export class AppModule {}
